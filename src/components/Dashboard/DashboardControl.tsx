92931664b09ca47f0df025873cf16dd90ce3f41e
Konsta Puutio <konsta.puutio@nordicsemi.no>
2026-02-24 12:51:15 +0200

    DashboardControl.tsx: Delete unneeded parameters to LDOCard

--- src/components/Dashboard/DashboardControl.tsx
+++ src/components/Dashboard/DashboardControl.tsx
@@ -92,13 +92,9 @@ export default ({ active }: PaneProps) => {
                     <LDOCard
                         ldo={ldo}
                         ldoModule={npmDevice.ldoModule[index]}
-                        cardLabel={
-                            ldos.length === 1 ? `Load Switch/LDO` : undefined
-                        }
                         key={`Ldo${1 + index}`}
                         disabled={disabled}
                         defaultSummary
-                        numberOfGPIOs={npmDevice.gpioModule.length}
                     />
                 ))}
         </MasonryLayout>
