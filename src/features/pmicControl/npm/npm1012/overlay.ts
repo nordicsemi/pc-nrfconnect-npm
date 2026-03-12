4209e6f84be2ef41edb6cd598c7c9d487f14f274
Konsta Puutio <konsta.puutio@nordicsemi.no>
2026-03-12 08:06:58 +0200

    npm1012/overlay: Delete overlay for LED until format is known

--- src/features/pmicControl/npm/npm1012/overlay.ts
+++ src/features/pmicControl/npm/npm1012/overlay.ts
@@ -4,33 +4,10 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import { LED, LEDMode, NpmExportLatest } from '../types';
+import { NpmExportLatest } from '../types';
 import type Npm1012 from './pmic1012Device';
 
-const ledModeToOverlay = (mode: LEDMode) => {
-    switch (mode) {
-        case 'Charger error':
-            return 'error';
-        case 'Charging':
-            return 'charging';
-        case 'Host':
-            return 'host';
-    }
-};
-
-const generateLEDs = (leds: LED[], deviceType: string) => `
-${deviceType}_ek_leds: leds {
-    compatible = "nordic,${deviceType}-led";
-    ${leds
-        .map(
-            (led, index) =>
-                `nordic,led${index}-mode = "${ledModeToOverlay(led.mode)}";`,
-        )
-        .join('    \n')}
-};
-`;
-
-export default (npmConfig: NpmExportLatest, npmDevice: Npm1012) => `/*
+export default (_npmConfig: NpmExportLatest, npmDevice: Npm1012) => `/*
 * Copyright (C) 2025 Nordic Semiconductor ASA
 * SPDX-License-Identifier: Apache-2.0
 */
@@ -42,7 +19,5 @@ export default (npmConfig: NpmExportLatest, npmDevice: Npm1012) => `/*
    ${npmDevice.deviceType}_ek_pmic: pmic@6b {
        compatible = "nordic,${npmDevice.deviceType}";
        reg = <0x6b>;
-
-       ${generateLEDs(npmConfig.leds, npmDevice.deviceType)}
    };
 };`;
