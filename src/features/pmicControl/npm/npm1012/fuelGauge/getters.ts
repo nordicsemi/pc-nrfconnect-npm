eb8eee7451f84127e3921d551b506acb4b987204
Konsta Puutio <konsta.puutio@nordicsemi.no>
2026-03-16 10:25:38 +0200

    npm1012: Add fuelGauge module

---
+++ src/features/pmicControl/npm/npm1012/fuelGauge/getters.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
+ */
+
+export class FuelGaugeGet {
+    constructor(
+        private sendCommand: (
+            command: string,
+            onSuccess?: (response: string, command: string) => void,
+            onError?: (response: string, command: string) => void,
+        ) => void,
+    ) {}
+
+    all() {
+        this.enabled();
+        this.activeBatteryModel();
+        this.storedBatteryModel();
+    }
+
+    enabled() {
+        this.sendCommand('fuel_gauge get');
+    }
+
+    activeBatteryModel() {
+        this.sendCommand(`fuel_gauge model get`);
+    }
+
+    storedBatteryModel() {
+        this.sendCommand(`fuel_gauge model list`);
+    }
+}
